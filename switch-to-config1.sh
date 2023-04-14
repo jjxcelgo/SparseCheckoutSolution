a76fe4708e252af9bc9d30021cd953c4556fde58
Jens Borg Juelsbak <jj@xcelgo.com>
2023-04-14 10:36:08 +0200

    ipsum

--- switch-to-config1.sh
+++ switch-to-config1.sh
@@ -2,10 +2,7 @@
 # Switch to sparse-checkout configuration 1
 
 # Ensure git sparse-checkout is enabled
-git config --global core.sparseCheckout true
+git sparse-checkout init
 
-# Replace the current .sparse-checkout file with config1
-cp sparse-checkout-config1 .git/info/sparse-checkout
-
-# Update the working tree
-git read-tree -m -u HEAD
+# Set the sparse-checkout configuration
+git sparse-checkout set -f --stdin < sparse-checkout-config1
--- switch-to-config2.sh
+++ switch-to-config2.sh
@@ -2,10 +2,7 @@
 # Switch to sparse-checkout configuration 2
 
 # Ensure git sparse-checkout is enabled
-git config --global core.sparseCheckout true
+git sparse-checkout init
 
-# Replace the current .sparse-checkout file with config2
-cp sparse-checkout-config2 .git/info/sparse-checkout
-
-# Update the working tree
-git read-tree -m -u HEAD
+# Set the sparse-checkout configuration
+git sparse-checkout set -f --stdin < sparse-checkout-config2
