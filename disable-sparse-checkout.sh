a33b07e61b1086035c0302bc6b9ff80f70713a1d
Jens Borg Juelsbak <jj@xcelgo.com>
2023-04-14 10:32:54 +0200

    lorem

--- disable-sparse-checkout.sh
+++ disable-sparse-checkout.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 # Disable sparse-checkout and show the entire repository
 
-# Turn off sparse-checkout
-git config --global core.sparseCheckout false
-
-# Remove the .sparse-checkout file
-rm -f .git/info/sparse-checkout
-
-# Reset the working tree to the last commit
-git reset --hard HEAD
-
-# Update the working tree
-git checkout HEAD
+# Disable sparse-checkout
+git sparse-checkout disable
